3189f1ce18d55ec9adb7ae6f8bcecfbaff61fe67
mj0323 <jackjemmy72@gmail.com>
2022-10-24 16:02:40 +0800

    Initial Commit

---
+++ book1/book1App.swift
@@ -0,0 +1,17 @@
+//
+//  book1App.swift
+//  book1
+//
+//  Created by User05 on 2022/10/24.
+//
+
+import SwiftUI
+
+@main
+struct book1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
